76a9c146bc64fbc15d472da9ec01358622899deb
MaxFray8908 <nadja.gawrilowa@yandex.ru>
2023-04-19 16:19:48 +0400

    refactor: update CSS according to BEM to rule 2.1 in html-and-css-extended.md and to rules 3.1, 3.2, 3.3 in html-and-css.md

--- app.js
+++ app.js
@@ -8,7 +8,7 @@
 
 // Event handling, user interaction is what starts the code execution.
 
-var taskInput=document.getElementById("new-task");//Add a new task.
+var taskInput=document.querySelector(".section-add__item");//Add a new task.
 var addButton=document.getElementsByTagName("button")[0];//first button
 var incompleteTaskHolder=document.querySelector(".list_todo");//ul of #incompleteTasks
 var completedTasksHolder=document.querySelector(".list_completed");//completed-tasks
